9912de18e709f4c7cf0ce0b8ca0c64ca5d7e1dc5
MSergey86 <105745751+MSergey86@users.noreply.github.com>
2023-05-16 10:36:31 +0300

    Update matchers.js

--- src/js/matchers.js
+++ src/js/matchers.js
@@ -1,19 +1,3 @@
-export function healthHero (hero) {
-    const countHero = hero.length;
-    let result = [];
-
-        for (let j = 0; j < countHero; j ++ ) {
-          let max = 0;
-          let indexMax = 0;
-            for (let i = 0; i < hero.length; i ++) {
-              if (hero[i].health > max) {
-                max = hero[i].health;
-                indexMax = i;
-              }
-            }
-          result.push(hero[indexMax])
-          hero.splice(indexMax, 1);
-      }
-      
-    return result;
+export function healthHero (hero) {     
+    return hero.sort((hero1, hero2) => hero2.health - hero1.health);
 }
